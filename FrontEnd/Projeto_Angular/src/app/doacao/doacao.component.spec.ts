ce9141a0b0234137627b718bb4451a375534097c
lari-sant <teixeiralary99@gmail.com>
2020-07-05 13:23:38 -0300

    imagens de doação e alts

--- FrontEnd/Projeto_Angular/src/app/doacao/doacao.component.spec.ts
+++ FrontEnd/Projeto_Angular/src/app/doacao/doacao.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DoacaoComponent } from './doacao.component';
+import { DoacaoComponent } from './doacao.component'
 
 describe('DoacaoComponent', () => {
   let component: DoacaoComponent;
--- FrontEnd/Projeto_Angular/src/app/doacao/doacao.component.ts
+++ FrontEnd/Projeto_Angular/src/app/doacao/doacao.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DoacaoService } from '../services/doacao.service';
+import { Doacao } from '../models/doacao';
+import { Usuario } from '../models/usuario';
+import { UsuarioService } from '../services/usuario.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-doacao',
@@ -7,8 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoacaoComponent implements OnInit {
 
-  constructor() { }
+  doacao: Doacao = new Doacao;
+  usuario: Usuario = new Usuario;
+  doacoes: Doacao[] = [];
+  key: string = 'data';
+  reverse: boolean = true;
+
+  constructor(private doacaoService: DoacaoService, private usuarioService: UsuarioService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    window.scroll(0, 0);
+    let idDoUsuario = this.route.snapshot.params['id'];
+    this.obterUsuarioPorId(idDoUsuario);
+    this.obterDoacoes(idDoUsuario);
+  }
+
+  obterUsuarioPorId(id: number) {
+    this.usuarioService.obterPorId(id).subscribe((resp: Usuario) => {
+      this.usuario = resp
+    })
+  }
+
+  obterDoacoes(idDoUsuario: number) {
+    this.doacaoService.obterPorUsuario(idDoUsuario).subscribe((resp: Doacao[]) => {
+      this.doacoes = resp;
+    });
+  }
+
+  doar() {
+    this.doacao.usuario = this.usuario;
+    this.doacao.codigo = Math.random().toString(36).substr(2, 9) + Math.floor(Math.random() * 10);
+
+    this.doacaoService.cadastrarDoacao(this.doacao)
+      .subscribe((resp: Doacao) => {
+        alert("Agora falta pouco...\nApresente o código abaixo no local de doação escolhido!\n\n" + this.doacao.codigo + "\n\nAgradecemos desde já!");
+        this.doacao = resp;
+        this.router.navigate(['/feed'])
+      })
   }
 }
